4ff65dda57496654cea52f2e26ddb7705851c725
wktan <tanweikang_96@hotmail.com>
2020-08-05 17:36:25 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         changed againe there!
+         changed on feature branch there!
         </p>
         <a
           className="App-link"
